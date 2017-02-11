fa4294034b9e2c7be9b3a70a78f94578b3976c19
Tim Pinkerton <timpinkerton@mail.com>
2017-02-10 22:35:20 -0500

    Updated mystyles.css and main.html

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.variable-width').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+  });
+});
