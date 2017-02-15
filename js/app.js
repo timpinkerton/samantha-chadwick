5abac655afc3446bf9f712adb9d81b217004e92e
Tim Pinkerton <timpinkerton@mail.com>
2017-02-14 23:12:42 -0500

    testing smoothdivscroll in editorial.html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 
 $(document).ready(function(){
-  $('.variable-width').slick({
-  dots: false,
-  infinite: true,
-  centermode: false,
-  variableWidth: true,
-  initialSlide: 3,
+  $('#makeMeScrollable').smoothDivScroll({
+    mousewheelScrolling: "allDirections",
+    manualContinuousScrolling: true,
+    autoScrollingMode: "onStart"
   });
 });
