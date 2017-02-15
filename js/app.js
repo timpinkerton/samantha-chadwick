406b43fee8341af08ac265338d33b2e7c5a2a297
Tim Pinkerton <timpinkerton@mail.com>
2017-02-14 21:57:59 -0500

    updates to editorial.html

--- js/app.js
+++ js/app.js
@@ -3,12 +3,8 @@ $(document).ready(function(){
   $('.variable-width').slick({
   dots: false,
   infinite: true,
-  speed: 300,
-  slidesToShow: 1,
+  centermode: false,
   variableWidth: true,
-  touchMove: true,
-  arrows: false,
-  centerMode: false,
-  swipe: true,
+  initialSlide: 3,
   });
 });
