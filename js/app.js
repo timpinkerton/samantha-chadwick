e5efd3571efc060bc103c8d5783dbb10b48015d9
Tim Pinkerton <timpinkerton@mail.com>
2017-02-16 22:00:20 -0500

    updated editorial.html

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   $('#makeMeScrollable').smoothDivScroll({
     mousewheelScrolling: "allDirections",
+    mousewheelScrollingStep: 3,
     manualContinuousScrolling: true,
     autoScrollingMode: "onStart"
   });
