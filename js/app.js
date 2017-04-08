274ffe114698fa1c63e645f01241a9d99feaee21
Tim Pinkerton <timpinkerton@mail.com>
2017-04-08 12:28:02 -0400

    testing js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 
 $(document).ready(function(){
   $("#makeMeScrollable").smoothDivScroll({
+    
     mousewheelScrolling: "allDirections",
     mousewheelScrollingStep: 1, 
+    
     manualContinuousScrolling: true,
+   
     touchScrolling: true,
+    
     hotSpotScrolling: false,
     
 //    hotSpotScrollingStep: 5,
